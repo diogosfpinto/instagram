114440c883813d5526721090efc369724b145411
Diogo S F Pinto <diogo.sfpinto@gmail.com>
2020-04-08 15:19:16 -0400

    cadastrando usuários no firebase

--- app/src/main/java/com/diogopinto/instagram/activity/CadastroActivity.java
+++ app/src/main/java/com/diogopinto/instagram/activity/CadastroActivity.java
@@ -1,16 +1,124 @@
 package com.diogopinto.instagram.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.diogopinto.instagram.R;
+import com.diogopinto.instagram.helper.ConfiguracaoFirebase;
+import com.diogopinto.instagram.model.Usuario;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 public class CadastroActivity extends AppCompatActivity {
+
+    EditText campoNome, campoEmail, campoSenha;
+    Button botaoCadastrar;
+    ProgressBar progressBar;
+
+    private Usuario usuario;
+
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+        botaoCadastrar = findViewById(R.id.buttonCadastrar);
+        progressBar = findViewById(R.id.progressCadastro);
+
+        progressBar.setVisibility(View.GONE);
+        inicializarComponentes();
+
+        botaoCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String nome = campoNome.getText().toString();
+                String email = campoEmail.getText().toString();
+                String senha = campoSenha.getText().toString();
+
+                if (!nome.isEmpty()){
+                    if (!email.isEmpty()){
+                        if (!senha.isEmpty()){
+                            usuario = new Usuario();
+                            usuario.setNome(nome);
+                            usuario.setEmail(email);
+                            usuario.setSenha(senha);
+                            cadastrar(usuario);
+
+                        } else {
+                            Toast.makeText(CadastroActivity.this,
+                                    "Campo 'SENHA' está vazio! Preencha-o!", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(CadastroActivity.this,
+                                "Campo 'EMAIL' está vazio! Preencha-o!", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(CadastroActivity.this,
+                            "Campo 'NOME' está vazio! Preencha-o!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+/**
+ * Método para cadastrar usuário e responsável pelo manipulação no Firebase*/
+    private void cadastrar(Usuario user){
+        progressBar.setVisibility(View.VISIBLE);
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        autenticacao.createUserWithEmailAndPassword(
+                usuario.getEmail(),usuario.getSenha()).addOnCompleteListener(
+                this,
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()){
+                            progressBar.setVisibility(View.GONE);
+                            Toast.makeText(CadastroActivity.this,
+                                    "Cadastro com sucesso", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            finish();
+
+                        }else {
+                            progressBar.setVisibility(View.GONE);
+                            String erroExcecao;
+                            try {
+                                throw task.getException();
+                            } catch(FirebaseAuthWeakPasswordException e){
+                                erroExcecao = "Digite uma senha mais forte!";
+                            } catch(FirebaseAuthInvalidCredentialsException e){
+                                erroExcecao = "Por favor, digite um e-mail válido";
+                            } catch(FirebaseAuthUserCollisionException e){
+                                erroExcecao = "Esta conta já foi cadastrada";
+                            }catch (Exception e){
+                                erroExcecao = "ao cadastrar usuário: "+e.getMessage();
+                                e.printStackTrace();
+                            }
+                            Toast.makeText(CadastroActivity.this, erroExcecao, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
+    }
+
+    public void inicializarComponentes(){
+        campoNome = findViewById(R.id.editCadastroNome);
+        campoEmail = findViewById(R.id.editCadastroEmail);
+        campoSenha = findViewById(R.id.editCadastroSenha);
     }
 }
---
+++ app/src/main/java/com/diogopinto/instagram/helper/ConfiguracaoFirebase.java
@@ -0,0 +1,25 @@
+package com.diogopinto.instagram.helper;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ConfiguracaoFirebase {
+    private static DatabaseReference referenciaFirebase;
+    private static FirebaseAuth referenciaAutenticacao;
+
+    //retorna a instancia do FirebaseAuth
+    public static FirebaseAuth getFirebaseAutenticacao(){
+        if (referenciaAutenticacao == null){
+            referenciaAutenticacao = FirebaseAuth.getInstance();
+        }
+        return referenciaAutenticacao;
+    }
+
+    public static DatabaseReference getFirebase(){
+        if (referenciaFirebase == null){
+            referenciaFirebase = FirebaseDatabase.getInstance().getReference();
+        }
+        return referenciaFirebase;
+    }
+}
