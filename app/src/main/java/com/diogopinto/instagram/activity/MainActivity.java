b0563163c88c7e93a2a02981854c667cab04c890
Diogo S F Pinto <diogo.sfpinto@gmail.com>
2020-04-14 16:59:57 -0400

    Logout tela principal

--- app/src/main/java/com/diogopinto/instagram/activity/MainActivity.java
+++ app/src/main/java/com/diogopinto/instagram/activity/MainActivity.java
@@ -1,16 +1,55 @@
 package com.diogopinto.instagram.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.diogopinto.instagram.R;
+import com.diogopinto.instagram.helper.ConfiguracaoFirebase;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth autenticacao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//      Configuração de objetos
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
+//        configurar toolbar
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("Instagram 2");
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.menu_sair:
+                deslogarUsuario();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void deslogarUsuario(){
+        autenticacao.signOut();
     }
 }
