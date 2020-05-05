264cd21da69a2e0e36945dfad6959d429f861eeb
Diogo S F Pinto <diogo.sfpinto@gmail.com>
2020-05-04 23:47:20 -0400

    activity editar perfil criada

---
+++ app/src/main/java/com/diogopinto/instagram/activity/EditarPerfilActivity.java
@@ -0,0 +1,16 @@
+package com.diogopinto.instagram.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.diogopinto.instagram.R;
+
+public class EditarPerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_perfil);
+    }
+}
--- app/src/main/java/com/diogopinto/instagram/fragment/PerfilFragment.java
+++ app/src/main/java/com/diogopinto/instagram/fragment/PerfilFragment.java
@@ -1,6 +1,7 @@
 package com.diogopinto.instagram.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,12 +11,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.diogopinto.instagram.R;
+import com.diogopinto.instagram.activity.EditarPerfilActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -38,8 +39,27 @@ public class PerfilFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_perfil, container, false);
+
+        textPublicacoes = view.findViewById(R.id.textPublicacoes);
+        textSeguidores = view.findViewById(R.id.textSeguidores);
+        textSeguindo = view.findViewById(R.id.textSeguindo);
+        buttonEditarPerfil = view.findViewById(R.id.buttonEditarPerfil);
+        progressBar = view.findViewById(R.id.progressBarPerfil);
+        gridViewPerfil = view.findViewById(R.id.gridViewPerfil);
+        avatarPerfil = view.findViewById(R.id.avatarPerfil);
+
+        //Abre edição de perfil
+        buttonEditarPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), EditarPerfilActivity.class);
+                startActivity(i);
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false);
+        return view;
     }
 
 }
