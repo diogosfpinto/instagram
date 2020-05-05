f73d258ed1066f29e0ecfc4ca91479c69952bb99
Diogo S F Pinto <diogo.sfpinto@gmail.com>
2020-05-04 23:19:57 -0400

    componentes do fragment Perfil criados

--- app/src/main/java/com/diogopinto/instagram/fragment/PerfilFragment.java
+++ app/src/main/java/com/diogopinto/instagram/fragment/PerfilFragment.java
@@ -4,10 +4,16 @@ package com.diogopinto.instagram.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.diogopinto.instagram.R;
 
@@ -15,6 +21,13 @@ import com.diogopinto.instagram.R;
  * A simple {@link Fragment} subclass.
  */
 public class PerfilFragment extends Fragment {
+
+    private TextView textSeguidores, textPublicacoes, textSeguindo;
+    private CircleImageView avatarPerfil;
+    private Button buttonEditarPerfil;
+    private ProgressBar progressBar;
+    private GridView gridViewPerfil;
+
 
 
     public PerfilFragment() {
